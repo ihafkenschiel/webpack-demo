18651ef3cb5c7b31c7367b2088defed110600236
Ian Hafkenschiel <ih143f@us.att.com>
2021-08-03 17:57:49 -0700

    Cleaned up workspce for output management tutorial

--- webpack.config.js
+++ webpack.config.js
@@ -7,28 +7,4 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,                        // regex, i=case-insensitive, $ means end of string
-                use: ['style-loader', 'css-loader'],    // in order
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: 'asset/resource',
-            },
-            {
-                test: /\.(csv|tsv)$/i,
-                use: ['csv-loader'],
-            },
-            {
-                test: /\.xml$/i,
-                use: ['xml-loader'],
-            }
-        ],
-    },
 };
